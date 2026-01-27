3c0a6f8e63bc9db999d5a28b510eca74f2614c72
Mark <markistevens@hotmail.com>
2026-01-27 01:24:11 -0500

    Updated the javascript and html files that I was working on. The javascript files are relatively complete, the HTML files are hardly touched and need to be done still.

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
-const urlBase = 'https://christianartigas.com/LAMPAPI';
+const urlBase = 'https://home.markstevens.tech/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
@@ -13,12 +13,12 @@ function doLogin()
 	
 	let login = document.getElementById("loginName").value;
 	let password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
+	var hash = md5( password );
 	
 	document.getElementById("loginResult").innerHTML = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
+	//let tmp = {login:login,password:password};
+	var tmp = {login:login,password:hash};
 	let jsonPayload = JSON.stringify( tmp );
 	
 	let url = urlBase + '/Login.' + extension;
@@ -35,7 +35,7 @@ function doLogin()
 				let jsonObject = JSON.parse( xhr.responseText );
 				userId = jsonObject.id;
 		
-				if( userId < 1 )
+				if( userId < 0 )
 				{		
 					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 					return;
@@ -46,7 +46,7 @@ function doLogin()
 
 				saveCookie();
 	
-				window.location.href = "color.html";
+				window.location.href = "contacts.html";
 			}
 		};
 		xhr.send(jsonPayload);
@@ -108,78 +108,60 @@ function doLogout()
 	window.location.href = "index.html";
 }
 
-function addColor()
+function doSignup()
 {
-	let newColor = document.getElementById("colorText").value;
-	document.getElementById("colorAddResult").innerHTML = "";
-
-	let tmp = {color:newColor,userId,userId};
+	// Get the values from the signup form 
+	let userName = document.getElementById("signupUserName").value;
+	let firstName = document.getElementById("signupFirstName").value;
+	let lastName = document.getElementById("signupLastName").value;
+	let password = md5(document.getElementById("signupPassword").value); 
+	
+	// Create a temporary object to hold the values
+	let tmp = {Login:userName,
+		FirstName:firstName,
+		LastName:lastName,
+		Password:password};
+	
+	// Convert the object to a JSON string
 	let jsonPayload = JSON.stringify( tmp );
 
-	let url = urlBase + '/AddColor.' + extension;
-	
+	// Create the URL for the signup API
+	let url = urlBase + '/Signup.' + extension;
+
+	// Create a new XMLHttpRequest object
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
+		// Handle the response from the API
 		xhr.onreadystatechange = function() 
 		{
+			// If the request is complete and the status is 200 and there is no error, display a success message
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
-	}
-	
-}
-
-function searchColor()
-{
-	let srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
-	
-	let colorList = "";
-
-	let tmp = {search:srch,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
-
-	let url = urlBase + '/SearchColors.' + extension;
-	
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+				// Parse the response from the API
 				let jsonObject = JSON.parse( xhr.responseText );
-				
-				for( let i=0; i<jsonObject.results.length; i++ )
+				// If there is no error, display a success message
+				if( jsonObject.error == "" )
 				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
+					document.getElementById("signupResult").innerHTML = "Signup successful";
 				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
+
+				// If there is an error, display the error message
+				else
+				{
+					document.getElementById("signupResult").innerHTML = jsonObject.error;
+				}
 			}
 		};
+		// Send the request to the API
 		xhr.send(jsonPayload);
 	}
+	// If there is an error, display the error message
 	catch(err)
 	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
+		document.getElementById("signupResult").innerHTML = err.message;
 	}
-	
 }
+
---
+++ js/contacts.js
@@ -0,0 +1,302 @@
+// Function to load the page and search for contacts. Called on initial page load 
+function loadPage() 
+{
+    // Read the cookie to get the user ID and name (stores to global variable from code.js)
+    readCookie();
+
+    // If the user ID is less than 0, redirect to the index page
+    if( userId < 0 )
+    {
+        window.location.href = "index.html";
+    }
+    // Start searching the contacts to load them. 
+    searchContacts();
+}
+
+// Helper function to escape HTML and prevent XSS attacks (written by Cursor) 
+function escapeHtml(text) {
+    const div = document.createElement('div');
+    div.textContent = text;
+    return div.innerHTML;
+}
+
+// Function to search for contacts / load the contacts into the table
+// A semi-substantial part of function was written / assisted by Cursor
+function searchContacts()
+{
+    
+    // Get the search text from the search input field
+    let srch = document.getElementById("searchText").value;
+	let contactList = "";
+	
+	// Clear previous result messages
+	document.getElementById("contactSearchResult").innerHTML = "";
+
+    // Create a temporary object to hold the values
+	let tmp = {search:srch,userId:userId};
+
+    // Convert the object to a JSON string
+	let jsonPayload = JSON.stringify( tmp );
+
+    // Create the URL for the search contacts API
+    let url = urlBase + '/SearchContacts.' + extension;
+
+    // Create a new XMLHttpRequest object
+	let xhr = new XMLHttpRequest();
+    // Open the request with the URL and set the content type to JSON
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    // Handle the response from the API
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				// Parse the response from the API
+				let jsonObject = JSON.parse( xhr.responseText );
+                // Get the results from the API
+                let results = jsonObject.results;
+                
+                // Check if results exist and handle empty results
+                if (!results || results.length === 0) {
+                    document.getElementById("contactTableBody").innerHTML = "<tr><td colspan='6'>No contacts found</td></tr>";
+                    document.getElementById("contactSearchResult").innerHTML = "0 contact(s) loaded";
+                    return;
+                }
+                
+                // Loop through the results
+                for( let i=0; i<results.length; i++ )
+                {
+                    // Build the row with escaped HTML to prevent XSS
+                    contactList += "<tr>";
+                    contactList += "<td>" + escapeHtml(results[i].FirstName) + "</td>";
+                    contactList += "<td>" + escapeHtml(results[i].LastName) + "</td>";
+                    contactList += "<td>" + escapeHtml(results[i].Phone) + "</td>";
+                    contactList += "<td>" + escapeHtml(results[i].Email) + "</td>";
+                    
+                    // Add Edit/Delete Buttons (passing the specific Contact ID to them)
+                    contactList += '<td><button onclick="openEditModal(\'' + results[i].ID + '\', this)">Edit</button></td>';
+                    contactList += '<td><button onclick="deleteContact(\'' + results[i].ID + '\')">Delete</button></td>';
+                    
+                    // Close the row
+                    contactList += "</tr>";
+                }
+                
+                // Display the contact list
+                document.getElementById("contactTableBody").innerHTML = contactList;
+                
+                // Display success message with count
+                document.getElementById("contactSearchResult").innerHTML = results.length + " contact(s) loaded";
+			}
+		};
+        // Send the request to the API
+		xhr.send(jsonPayload);
+	}
+	// If there is an error, display the error message
+	catch(err)
+	{
+		// Display the error message
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+}
+
+// Function to delete a contact
+// Partially written by Cursor 
+function deleteContact(contactID)
+{
+    // Clear the previous result message
+    document.getElementById("contactDeleteResult").innerHTML = "";
+
+    // Show confirmation popup before deleting
+    if (!confirm("Are you sure you want to delete this contact?")) {
+        return;
+    }
+    // Create a temporary object + JSON payload to send to the API 
+    let tmp = {ID:contactID,UserID:userId};
+    let jsonPayload = JSON.stringify( tmp );
+
+    // Create the URL for the delete contact API
+    let url = urlBase + '/DeleteContact.' + extension;
+
+    // Create a new XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        // Handle the response from the API
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                // Display the success message
+                document.getElementById("contactDeleteResult").innerHTML = "Contact has been deleted";
+
+                // Reload the contacts to update with contact deleted.  
+                searchContacts(); 
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        // Display the error message
+        document.getElementById("contactDeleteResult").innerHTML = err.message;
+    }
+}
+
+// Function to add a contact 
+// Partially written by Cursor / Gemini
+function addContact()
+{
+    // Clear the previous result message
+    document.getElementById("contactAddResult").innerHTML = "";
+
+    // Get the values from contact form and create a JSON payload. 
+	let newContactFN = document.getElementById("contactFirstName").value;
+	let newContactLN = document.getElementById("contactLastName").value;
+	let newContactPhone = document.getElementById("contactPhone").value;
+	let newContactEmail = document.getElementById("contactEmail").value;
+    if( newContactFN == "" || newContactLN == "" || newContactPhone == "" || newContactEmail == "" )
+    {
+        document.getElementById("contactAddResult").innerHTML = "All fields are required";
+        return;
+    }
+	let tmp = {FirstName:newContactFN,
+        LastName:newContactLN,
+        Phone:newContactPhone,
+        Email:newContactEmail,
+        UserID:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+    // Create the URL for the add contact API
+    let url = urlBase + '/AddContact.' + extension;
+
+    // Create a new XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        // Handle the response from the API
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                // Display the success message
+                document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+
+                // Reload the contacts to update with new contact added.  
+                searchContacts(); 
+
+                // Close the add contact modal
+                document.getElementById("addContactModal").style.display = "none";
+
+                // Clear the add contact form
+                document.getElementById("contactFirstName").value = "";
+                document.getElementById("contactLastName").value = "";
+                document.getElementById("contactPhone").value = "";
+                document.getElementById("contactEmail").value = "";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        // Display the error message
+        document.getElementById("contactAddResult").innerHTML = err.message;
+    }
+}
+
+// Function to open the edit modal and populate it with existing contact data
+// Mostly written by cursor, some help from Gemini
+function openEditModal(id, button)
+{
+    // Save the ID for later (when the user clicks "Save")
+    document.getElementById("editContactId").value = id;
+
+    // Find the row that holds this button
+    let row = button.parentNode.parentNode;
+
+    // Get the text from the cells (0=First, 1=Last, 2=Phone, 3=Email)
+    let first = row.cells[0].innerHTML;
+    let last  = row.cells[1].innerHTML;
+    let phone = row.cells[2].innerHTML;
+    let email = row.cells[3].innerHTML;
+
+    // Fill the "Edit" boxes
+    document.getElementById("editContactFirstName").value = first;
+    document.getElementById("editContactLastName").value = last;
+    document.getElementById("editContactPhone").value = phone;
+    document.getElementById("editContactEmail").value = email;
+
+    // Show the popup
+    document.getElementById("editContactModal").style.display = "block";
+}
+
+// Function to submit the edited contact (called when user clicks "Save" in the modal)
+// Heavily assisted by cursor, some help from Gemini 
+function editContact()
+{
+    // Clear the previous result message
+    document.getElementById("contactEditResult").innerHTML = "";
+
+    // Get the ID from the hidden input field in the edit contact modal
+    let contactID = document.getElementById("editContactId").value;
+
+    // Get the values from contact form and create a JSON payload
+    let newContactFN = document.getElementById("editContactFirstName").value;
+    let newContactLN = document.getElementById("editContactLastName").value;
+    let newContactPhone = document.getElementById("editContactPhone").value;
+    let newContactEmail = document.getElementById("editContactEmail").value;
+
+    // Validate that all fields are filled out
+    if( newContactFN == "" || newContactLN == "" || newContactPhone == "" || newContactEmail == "" )
+    {
+        // Inform the user that all fields are required
+        document.getElementById("contactEditResult").innerHTML = "All fields are required";
+        return;
+    }
+
+    let tmp = {ID:contactID,
+        FirstName:newContactFN,
+        LastName:newContactLN,
+        Phone:newContactPhone,
+        Email:newContactEmail,
+        UserID:userId};
+    let jsonPayload = JSON.stringify( tmp );
+
+    // Create the URL for the edit contact API
+    let url = urlBase + '/EditContact.' + extension;
+
+    // Create a new XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        // Handle the response from the API
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                // Display the success message
+                document.getElementById("contactEditResult").innerHTML = "Contact has been edited";
+
+                // Reload the contacts to update with edited contact
+                searchContacts(); 
+
+                // Close the edit contact modal
+                document.getElementById("editContactModal").style.display = "none";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        // Display the error message
+        document.getElementById("contactEditResult").innerHTML = err.message;
+    }
+}
+
