f7a42f51b4f80789fb4100deb81fcad3e8cb1113
MarkS <markistevens@hotmail.com>
2026-01-28 19:42:28 +0000

    Updated AddContact

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -5,21 +5,13 @@
 	$userId = $inData["userId"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
-<<<<<<< HEAD
-<<<<<<<< HEAD:LAMPAPI/AddContact.php
 		$stmt = $conn->prepare("INSERT into Contacts (UserId,Name) VALUES(?,?)");
-========
-		$stmt = $conn->prepare("DELETE FROM Contacts WHERE UserId=$userId");
->>>>>>>> 4aae53f (Updated AddContact, SearchContact, and DeleteContact (WIP)):LAMPAPI/DeleteContact.php
-=======
-		$stmt = $conn->prepare("INSERT into Contacts (UserId,Name) VALUES(?,?)");
->>>>>>> 4aae53f (Updated AddContact, SearchContact, and DeleteContact (WIP))
 		$stmt->bind_param("ss", $userId, $contact);
 		$stmt->execute();
 		$stmt->close();
@@ -43,12 +35,5 @@
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
-<<<<<<< HEAD
-<<<<<<<< HEAD:LAMPAPI/AddContact.php
 
-========
->>>>>>>> 4aae53f (Updated AddContact, SearchContact, and DeleteContact (WIP)):LAMPAPI/DeleteContact.php
-=======
->>>>>>> 4aae53f (Updated AddContact, SearchContact, and DeleteContact (WIP))
 ?>
